0609c4676a235fcfe5888c12fad02fc2a0cc3477
Jarogniewa <marta.jeczen@o2.pl>
2020-10-04 10:26:24 +0200

    feat(toolbar: working toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,41 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {StartComponent} from './component/start/start.component';
+import {SelectCityComponent} from './component/select-city/select-city.component';
+import {ShowWeatherComponent} from './component/show-weather/show-weather.component';
+import {ToolbarComponent} from './component/toolbar/toolbar.component';
+import {PageNotFoundComponent} from './component/page-not-found/page-not-found.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
+const materialModules = [
+  MatToolbarModule,
+  MatButtonModule,
+  MatIconModule
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartComponent,
+    SelectCityComponent,
+    ShowWeatherComponent,
+    ToolbarComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ...materialModules
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
